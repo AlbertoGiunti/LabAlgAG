8b64954fb0f026ea915089be57f08aeccfb4dbbb
AlbertoGiunti <alberto.giunti@stud.unifi.it>
2023-05-11 21:47:54 +0200

    Errori corretti, ora devo fare la parte dei grafici

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def oll_tests(n, rep):
         random.shuffle(A)
         e = timeit.timeit(stmt=lambda: [oll.insert(A[i]) for i in A], number=1)
         eti = eti + e
-        k = random.randint(1, n)
+        # k = random.randint(1, n)
+        k = int(n / 2)
         os = timeit.timeit(stmt=lambda: oll.order_statistic(k), number=1)
         etos = etos + os
     # Calcolo dei tempi medi di inserimento e ricerca della k-esima statistica d'ordine
@@ -44,7 +45,8 @@ def bst_tests(n, rep):
         random.shuffle(A)
         e = timeit.timeit(stmt=lambda: [bst.insert(A[i]) for i in A], number=1)
         eti = eti + e
-        k = random.randint(1, n)
+        # k = random.randint(1, n)
+        k = int(n / 2)
         # Misura del tempo di ricerca della k-esima statistica d'ordine
         os = timeit.timeit(stmt=lambda: bst.get_kesimo(bst.root, k), number=1)
         etos = etos + os
@@ -68,7 +70,8 @@ def rbt_tests(n, rep):
         random.shuffle(A)
         e = timeit.timeit(stmt=lambda: [rbt.insert(A[i]) for i in A], number=1)
         eti = eti + e
-        k = random.randint(1, n)
+        # k = random.randint(1, n)
+        k = int(n / 2)
         os = timeit.timeit(stmt=lambda: rbt.os_select(rbt.root, k), number=1)
         etos = etos + os
     # Calcolo dei tempi medi di inserimento e ricerca della k-esima statistica d'ordine
@@ -78,10 +81,11 @@ def rbt_tests(n, rep):
 
 
 # Definiamo una lista di numeri di test
-n_values = list(range(1, 30))
+n_values = list(range(50, 1000, 50))
 rep = 15
 
 # Liste dei risultati dei tempi di inserimento
+assex = []
 oll_ins_results = []
 bst_ins_results = []
 rbt_ins_results = []
@@ -92,23 +96,25 @@ bst_os_results = []
 rbt_os_results = []
 
 for n in n_values:
+    assex.append(n)
+
     oll_tins, oll_tos = oll_tests(n, rep)
     oll_ins_results.append(oll_tins)
     oll_os_results.append(oll_tos)
-    print(oll_ins_results)
-    print(oll_os_results)
     bst_tins, bst_tos = bst_tests(n, rep)
     bst_ins_results.append(bst_tins)
     bst_os_results.append(bst_tos)
-    print(bst_ins_results)
-    print(bst_os_results)
     rbt_tins, rbt_tos = rbt_tests(n, rep)
     rbt_ins_results.append(rbt_tins)
     rbt_os_results.append(rbt_tos)
-    print(rbt_ins_results)
-    print(rbt_os_results)
 
 # Stampa i risultati trovati
-print("Risultati test inserimento lista ordinata:")
+print("Risultati dei test: ")
+print("OLL inserimento: "+str(oll_ins_results))
+print("OLL ordere-statistic: "+str(oll_os_results))
+print("ABR inserimento: "+str(bst_ins_results))
+print("ABR order-statistic: "+str(bst_os_results))
+print("ARN inserimento: "+str(rbt_ins_results))
+print("ARN order-statistic: "+str(rbt_os_results))
 
 # Rappresentiamo i risultati graficamente
--- ordered_linked_list.py
+++ ordered_linked_list.py
@@ -4,33 +4,37 @@ class Node:
         self.next = None
 
 
+# Lista linkata ordinata in senso crescente
 class OrderedLinkedList:
 
     def __init__(self):
         self.head = None
-        self.tail = None
 
     def insert(self, value):
         new_node = Node(value)
 
         if self.head is None:
             self.head = new_node
-            self.tail = new_node
-        elif value < self.head.value:
+        elif value <= self.head.value:
             new_node.next = self.head
             self.head = new_node
         else:
             curr_node = self.head
-            while curr_node.next is not None and value > curr_node.next.value:
+            while curr_node.next is not None and new_node.value > curr_node.next.value:
                 curr_node = curr_node.next
             new_node.next = curr_node.next
             curr_node.next = new_node
 
-            if new_node.next is None:
-                self.tail = new_node
-
     def order_statistic(self, k):
         curr_node = self.head
-        for i in range(k):
+        # cerco il nodo in posizione k
+        for i in range(0, k-1):
             curr_node = curr_node.next
         return curr_node.value
+
+    # Metodo per stampare la lista
+    def print_list(self):
+        curr_node = self.head
+        while curr_node is not None:
+            print(curr_node.value)
+            curr_node = curr_node.next
